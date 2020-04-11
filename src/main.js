b739219c2dcebff3aaca478c9fd060905675d6ee
julczka <56249914+julczka@users.noreply.github.com>
2020-04-11 21:53:22 +0200

    video player component

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueYoutube from "vue-youtube";
 
+Vue.use(VueYoutube);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="about">
     <h1>About me</h1>
+    <div class="youtube">
+      <video-player class="vjs-layout-medium" :options="videoOptions" />
+      <!-- <youtube
+        :video-id="videoId"
+        :player-vars="playerVars"
+        @playing="playing"
+      ></youtube> -->
+    </div>
     <div class="fill"></div>
     <SkillsGrid />
     <div class="fill"></div>
@@ -9,22 +17,59 @@
 
 <script>
 import SkillsGrid from "../components/SkillsGrid";
+import VideoPlayer from "../components/VideoPlayer";
 export default {
   components: {
-    SkillsGrid: SkillsGrid
+    SkillsGrid: SkillsGrid,
+    VideoPlayer
   },
   data() {
-    return {};
+    return {
+      videoOptions: {
+        autoplay: true,
+        controls: true,
+        fluid: true,
+        responsive: true,
+        breakpoints: {
+          tiny: 300,
+          xsmall: 400,
+          small: 500,
+          medium: 600,
+          large: 700,
+          xlarge: 800,
+          huge: 900
+        },
+        sources: [
+          {
+            src: require("../assets/julia_krysytyna_gruszczynska.mp4"),
+            type: "video/mp4"
+          }
+        ]
+      }
+      // videoId: "7AXsVXHfir0",
+      // playerVars: {
+      //   autoplay: 0,
+      //   showinfo: 0,
+      //   modestbranding: 1,
+      //   controls: 1,
+      //   rel: 0,
+      //   loop: 1
+      // }
+    };
   },
 
-  methods: {},
+  methods: {
+    playing() {
+      console.log(" we are watching!!!");
+    }
+  },
 
   mounted() {}
 };
 </script>
 
 <style lang="scss" scoped>
-// @import "@/styles/_variables.scss";
+// @import "@/styles/_video.scss";
 // $pink-light-hex: #f23558;
 .fill {
   height: 200px;
